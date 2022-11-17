9166a67ba2a016d892d048e12a88668bd6ae1f42
Suruthi Mohan Suren <suruthimohan@gmail.com>
2022-11-17 14:44:15 +0530

    initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Userdetails } from './userdetails';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formCreation';
+
+  genders = ["Male","Female"];
+
+  userData = new Userdetails("","","","","");
+
 }
---
+++ src/app/userdetails.ts
@@ -0,0 +1,11 @@
+export class Userdetails {
+    
+   constructor(
+        public username:string,
+        public fullname:string,
+        public email:string,
+        public password:string,
+        public gender:string
+   )
+   {}
+}
